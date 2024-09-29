2b5ca61f64348fc030b807748f50888877cd6f1c
CyrusNG <wu.cyrus@gmail.com>
2024-09-29 14:46:34 +0800

    [android] update market flow for all chief markets in china

--- src/android/Market.java
+++ src/android/Market.java
@@ -15,14 +15,31 @@ import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
+import android.content.pm.PackageManager;
+
 /**
- * Interact with Google Play.
+ * Interact with Markets.
  *
  * @author Cyrus NG
  * @license Apache 2.0
  */
 public class Market extends CordovaPlugin
 {
+    
+    /**
+     * will use the first market found in following market list
+     */
+    private static final List<String> MARKET_LSIT = List.of(
+        "com.xiaomi.market",                 //Mi
+        "com.bbk.appstore",                  //Vivo
+        "com.oppo.market",                   //Oppo
+        "com.hihonor.appmarket",             //Honor
+        "com.huawei.appmarket",              //Huawei
+        "com.meizu.mstore",                  //Meizu
+        "com.sec.android.app.samsungapps",   //Samsung
+        "com.android.vending"                //Google
+    )
+
     /**
      * Executes the request and returns PluginResult.
      *
@@ -37,23 +54,29 @@ public class Market extends CordovaPlugin
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         try {
+
+            // open api
             if (action.equals("open")) {
                 if (args.length() == 1) {
-                    String appId = args.getString(0);
-                    this.openGooglePlay(appId);
-
+                    Context context = this.cordova.getActivity().getApplicationContext();
+                    String appId = StringUtils.defaultIfEmpty(args.getString(0), context.getPackageName());
+                    String targetMarket = this.findMarket();
+                    this.openMarket(targetMarket, appId);
                     callbackContext.success();
                     return true;
                 }
+
+            // search api
             }else if (action.equals("search")) {
                 if (args.length() == 1) {
                     String key = args.getString(0);
-                    this.searchGooglePlay(key);
-
+                    String targetMarket = this.findMarket();
+                    this.searchMarket(targetMarket, key);
                     callbackContext.success();
                     return true;
                 }
             }
+
         } catch (JSONException e) {
             Log.d("CordovaLog","Plugin Market: cannot parse args.");
             e.printStackTrace();
@@ -66,30 +89,64 @@ public class Market extends CordovaPlugin
     }
 
     /**
-     * Open the appId details on Google Play .
+     * Open the appId details on market
      *
+     * @param marketPackage
+     *            market's package name
+     *            E.g.: com.xiaomi.market
      * @param appId
-     *            Application Id on Google Play.
+     *            Application Id on market
      *            E.g.: com.google.earth
      */
-    private void openGooglePlay(String appId) throws android.content.ActivityNotFoundException {
+    private void openMarket(String marketPackage, String appId) throws android.content.ActivityNotFoundException {
         Context context = this.cordova.getActivity().getApplicationContext();
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appId));
+        intent.setPackage(marketPackage);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
 
     /**
-     * search the details on Google Play .
+     * search the details on the market
      *
+     * @param marketPackage
+     *            market's package name
+     *            E.g.: com.xiaomi.market
      * @param searchKeyword
-     *            Application Id on Google Play.
+     *            Application Id on market
      *            E.g.: earth
      */
-    private void searchGooglePlay(String key) throws android.content.ActivityNotFoundException {
+    private void searchMarket(String marketPackage, String key) throws android.content.ActivityNotFoundException {
         Context context = this.cordova.getActivity().getApplicationContext();
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=" + key));
+        intent.setPackage(marketPackage);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
+    }
+
+    /**
+     * find market app
+     *
+     * @return A mark package name or null
+     */
+    private String findMarket() throws Exception {
+        // get context
+        Context context = this.cordova.getActivity().getApplicationContext();
+        // get package manager
+        final PackageManager packageManager = context.getPackageManager();
+        // package name variable
+        String targetPackage = null;
+        // loop to find one matched market app
+        for (String packageName : MARKET_LSIT) {
+            try {
+                //try to get application info
+                ApplicationInfo info = packageManager.getApplicationInfo(packageName, PackageManager.GET_UNINSTALLED_PACKAGES);
+                targetPackage = packageName;
+                return;
+            } catch (NameNotFoundException e) {
+                // nothing to do
+            }
+        }
+        throw new Exception("Not Found Market");
     }
 }
